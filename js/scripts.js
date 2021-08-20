905f5b433cd790615e37d71577050405640af9ed
DiegoSerafico <dseraficohernandez@gmail.com>
2021-08-20 09:53:33 -0700

    add form without functionality

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,8 @@ function beepBoop (num) {
   for (let i = 0; i <= num; i++) {
     if (i.toString().includes("3")) {
       result.push("Won't you be my neighbor?");
+    } else if (i.toString().includes("2")) {
+      result.push("Boop!");
     } else if (i.toString().includes("1")) {
       result.push("Beep!");
     } else {
